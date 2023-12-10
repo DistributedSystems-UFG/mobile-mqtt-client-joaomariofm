11c337a3c9dd4397ebb463d65a3a20f1259500ed
João Mário <joaomf17p2@gmail.com>
2023-12-10 15:33:00 -0300

    adding and configuring messages history list

--- app/src/main/java/com/example/basicandroidmqttclient/MainActivity.java
+++ app/src/main/java/com/example/basicandroidmqttclient/MainActivity.java
@@ -2,26 +2,35 @@ package com.example.basicandroidmqttclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.content.Intent;
+
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
+
 
 import com.hivemq.client.mqtt.datatypes.MqttQos;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.basicandroidmqttclient.MESSAGE";
-    public static final String brokerURI = "3.223.10.115";
+    public static final String brokerURI = "54.234.202.106";
 
     Activity thisActivity;
-    TextView subMsgTextView;
+
+    ListView simpleList;
+    List<String> messagesList = new ArrayList<>();
+    ArrayAdapter <String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,35 +38,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         thisActivity = this;
-        subMsgTextView = (TextView) findViewById(R.id.editTextMultiLineSubMsg);
+
+        simpleList = (ListView) findViewById(R.id.simpleListView);
+        arrayAdapter = new ArrayAdapter <String>(this, R.layout.list_item, R.id.textView, messagesList);
+        simpleList.setAdapter(arrayAdapter);
+
+        this.topicSubscription();
     }
 
-    /** Called when the user taps the Send button */
-    public void publishMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText topicName = (EditText) findViewById(R.id.editTextTopicName);
-        EditText value = (EditText) findViewById(R.id.editTextValue);
-
-        Mqtt5BlockingClient client = Mqtt5Client.builder()
-                .identifier(UUID.randomUUID().toString())
-                .serverHost(brokerURI)
-                .buildBlocking();
-
-        client.connect();
-        client.publishWith()
-                .topic(topicName.getText().toString())
-                .qos(MqttQos.AT_LEAST_ONCE)
-                .payload(value.getText().toString().getBytes())
-                .send();
-        client.disconnect();
-
-        String message = topicName.getText().toString() + " " + value.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-    public void sendSubscription(View view) {
-        EditText topicName = (EditText) findViewById(R.id.editTextTopicNameSub);
+    public void topicSubscription() {
+        String temperatureTopic = "/temperature-android";
+        String luminosityTopic = "/luminosity-android";
 
         Mqtt5BlockingClient client = Mqtt5Client.builder()
                 .identifier(UUID.randomUUID().toString())
@@ -66,20 +57,30 @@ public class MainActivity extends AppCompatActivity {
 
         client.connect();
 
-        // Use a callback lambda function to show the message on the screen
         client.toAsync().subscribeWith()
-                .topicFilter(topicName.getText().toString())
+                .topicFilter(temperatureTopic)
                 .qos(MqttQos.AT_LEAST_ONCE)
                 .callback(msg -> {
                     thisActivity.runOnUiThread(new Runnable() {
                         public void run() {
-                            subMsgTextView.setText(new String(msg.getPayloadAsBytes(), StandardCharsets.UTF_8));
+                            arrayAdapter.insert(new String(msg.getPayloadAsBytes(), StandardCharsets.UTF_8), arrayAdapter.getCount());
+                            arrayAdapter.notifyDataSetChanged();
+                        }
+                    });
+                })
+                .send();
+
+        client.toAsync().subscribeWith()
+                .topicFilter(luminosityTopic)
+                .qos(MqttQos.AT_LEAST_ONCE)
+                .callback(msg -> {
+                    thisActivity.runOnUiThread(new Runnable() {
+                        public void run() {
+                            arrayAdapter.insert(new String(msg.getPayloadAsBytes(), StandardCharsets.UTF_8), arrayAdapter.getCount());
+                            arrayAdapter.notifyDataSetChanged();
                         }
                     });
                 })
                 .send();
     }
-
-
-
 }
